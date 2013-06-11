8f5c6a1e4d54cd3ad39aaaf630663698a778dfb4
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-11 08:54:13 -0400

    externalizing update script and starting to properly handle user not found"

---
+++ scripts/update-github-repos.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var leveldb  =  require('valuepack-core/mine/leveldb')
+  , update   =  require('../lib/update-github-repos')
+  , argv     =  process.argv
+  , username =  argv[2];
+
+if (!username) {
+  console.error('Usage: update-github-repos <username>');
+  process.exit(1);
+}
+
+leveldb.open(function (err, db) {
+  if (err) return leveldb.close(err);
+  db = require('level-sublevel')(db);
+  update(db, username, function (err, res) {
+    if (err) console.error(err);
+    console.error('remaining', res.remaining);
+    leveldb.close();
+  });
+})
