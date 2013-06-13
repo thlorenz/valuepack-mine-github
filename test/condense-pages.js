6ea697b4aa9a215f754b2fff5cc9fb0a8d1cae14
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-12 22:33:47 -0400

    improving and completely testing condense pages module

--- test/condense-pages.js
+++ test/condense-pages.js
@@ -30,3 +30,17 @@ test('\ncondensing two pages with status code 304', function (t) {
   t.notOk(c.mixedModifieds, 'no mixed modifieds')
   t.end()
 })
+
+test('\ncondensing two pages first status code 304, second with status code 200', function (t) {
+  var uno = { headers: 'headers uno', statusCode: 304, body: 'body uno' }
+    , dos = { headers: 'headers dos', statusCode: 200, body: 'body dos' }
+
+  var c = condense([ uno, dos ])
+
+  t.equal(c.headers, 'headers dos', 'headers of last page')
+  t.equal(c.statusCode, 200, 'status code 200')
+  t.notOk(c.body, 'no body')
+  t.notOk(c.modified, 'modified')
+  t.ok(c.mixedModifieds, 'mixed modifieds')
+  t.end()
+})
