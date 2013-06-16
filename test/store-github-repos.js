9720974aa393ea8d5af0986d6506e7b36745bc01
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-16 16:39:26 -0400

    testing correct storing of users and repos

--- test/store-github-repos.js
+++ test/store-github-repos.js
@@ -4,9 +4,7 @@
 var test     =  require('tap').test
   , fs       =  require('fs')
   , dump     =  require('level-dump')
-  //, levelup  =  require('levelup')
-  //, leveldb  =  require('level-test')('valuepack-mine-github-test-store', { valueEncoding: 'json' })
-  , leveldb  =  require('valuepack-core/mine/leveldb-mem')
+  , level    =  require('level-test')({ mem: true })
   , sublevel =  require('level-sublevel')
   , store    =  require('../lib/store-github-repos')
 
@@ -16,20 +14,84 @@ var test     =  require('tap').test
 var json = fs.readFileSync(__dirname + '/fixtures/repos-isaacs.json', 'utf8')
 
 test('\nwhen storing user with 5 followers and 3 repos one of which is php', function (t) {
-  leveldb.open(function (err, db) {
-    db = sublevel(db)
+  var db = sublevel(level(null, { valueEncoding: 'json' }))
+  
+  store(db, json, function (err, res) {
+    t.plan(2)
 
+    t.notOk(err, 'no error')
     
-    store(db, json, function (err, res) {
-      t.notOk(err, 'no error')
+    t.test('\n# stores repos correctly', function (t) {
+      var repos = []
+      dump(
+          res.sublevels.githubRepos
+        , [].push.bind(repos)
+        , function (err) { 
+            if (err) console.error(err)
+            var fst = repos[0]
+              , snd = repos[1]
+
+            t.equal(fst.key, 'isaacs/abbrev-js', 'stores first JavaScript repo')
+            t.deepEqual(
+                fst.value
+              , { name      :  'abbrev-js',
+                  fullname  :  'isaacs/abbrev-js',
+                  forks     :  3,
+                  stars     :  21,
+                  issues    :  1,
+                  hasIssues :  true,
+                  language  :  'JavaScript',
+                  created   :  '2010-03-09T21:16:26Z',
+                  updated   :  '2013-06-07T22:04:42Z',
+                  owner     :  'isaacs' }
+              , 'stores name, fullname, forks, stars, issues, owner and dates correctly'
+            )
+
+            t.equal(snd.key, 'isaacs/arg-parse-js', 'stores second JavaScript repo')
+            t.deepEqual(
+                snd.value
+              , { name      :  'arg-parse-js',
+                  fullname  :  'isaacs/arg-parse-js',
+                  forks     :  1,
+                  stars     :  4,
+                  issues    :  0,
+                  hasIssues :  true,
+                  language  :  'JavaScript',
+                  created   :  '2010-02-24T19:47:15Z',
+                  updated   :  '2013-04-24T17:49:55Z',
+                  owner     :  'isaacs' }
+              , 'stores name, fullname, forks, stars, issues, owner and dates correctly'
+            )
+
+            t.equal(repos.length, 2, 'does not store php repo')
+            t.end()
+          }
+      )
+    })
+
+    test('\n# stores user correctly', function (t) {
+      var users = []
+      dump(
+          res.sublevels.githubUsers
+        , [].push.bind(users)
+        , function (err) {
+            if (err) console.error(err);
+            var fst = users[0]
+
+            t.equal(fst.key, 'isaacs', 'stores the user')
+            t.equal(users.length, 1, 'stores no other user')
+            t.equal(fst.value.followersCount, 5, 'correctly calculates and stores followers count')
+            t.deepEqual(
+                fst.value.followers
+              , [ 'dwcook', 'tomplays', 'jasonkostempski', 'jeffchuber', 'daaku' ]
+              , 'stores followers names'
+            )
+            t.equal(fst.value.id, 9287, 'stores user\'s id')
+
+            t.end()
+          }
+      )
       
-      dump.write = function (entries) {
-        console.error('entries: ', entries);
-      }
-      dump(res.sublevels.githubRepos, function (entries) { 
-        t.end()
-      });
-    });
-    
-  });
+    })
+  })
 })
