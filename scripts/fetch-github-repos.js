41a382694db8ea2035ea2b4e86f96facc857eb3c
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-17 07:53:39 -0400

    including starred repos during fetch

--- scripts/fetch-github-repos.js
+++ scripts/fetch-github-repos.js
@@ -7,6 +7,7 @@ var fetch =  require('../lib/fetch-github-repos')
   , query =  require('../lib/client-id-secret-query')
   , argv = process.argv
   , details = true
+  , starred = true
 
 var detailsIdx = argv.indexOf('--nodetails')
 if (~detailsIdx) {
@@ -14,17 +15,28 @@ if (~detailsIdx) {
   argv.slice(detailsIdx, 1)
 }
 
+var starredIdx = argv.indexOf('--nostarred')
+if (~starredIdx) {
+  starred = false
+  argv.slice(starredIdx, 1)
+}
+
 var user = process.argv[2]
 
 if(!user) {
-  console.log('Usage: ./fetch-github-repos isaacs [--nodetails]')
+  console.log('Usage: ./fetch-github-repos isaacs [--nodetails] [-nostarred]')
   process.exit(1)
 }
 
-console.error('Getting repos for: %s %s user details', user, details ? 'including' : 'excluding');
+function including(b) {
+  return b ? 'including' : 'excluding'
+}
+
+console.error('Getting repos for: %s %s user details and %s starred repos ', user, including(details), including(starred));
 fetch(
     { user           :  user
     , getUserDetails :  details
+    , getUserStarred :  starred
     , query          :  query
     }
   , function (err, res) {
