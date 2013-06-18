61f395b9acd3560265982de0f97c500c5094e819
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-18 07:55:38 -0400

    pulling out latest last modified when condensing pages

--- test/condense-pages.js
+++ test/condense-pages.js
@@ -44,3 +44,21 @@ test('\ncondensing two pages first status code 304, second with status code 200'
   t.ok(c.mixedModifieds, 'mixed modifieds')
   t.end()
 })
+
+test('\nthree headers, 2nd one has latest last modified', function (t) {
+
+  var moduno  =  new Date(10000)
+    , moddos  =  new Date(30000)
+    , modtres =  new Date(20000)
+    , uno     =  { headers: { page: 'uno', lastModified: moduno },   statusCode: 200, body: 'body uno' }
+    , dos     =  { headers: { page: 'dos', lastModified: moddos },   statusCode: 200, body: 'body dos' }
+    , tres    =  { headers: { page: 'tres', lastModified: modtres },  statusCode: 200, body: 'body tres' }
+
+  var c = condense([ uno, dos, tres ])
+  t.equal(c.headers.page, 'tres', 'headers of last page')
+  t.equal(c.headers.lastModified, moddos, 'last modified of header is last modified of 2nd page')
+  
+  t.equal(c.statusCode, 200, 'status code 200')
+
+  t.end()
+})
