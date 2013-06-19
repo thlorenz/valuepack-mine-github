dae05a32b7049e869d4de60f593a39ec62cbadf8
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-18 22:04:10 -0400

    exposing public api via index

---
+++ index.js
@@ -0,0 +1,5 @@
+exports.fetchGithubRepos    =  require('./lib/fetch-github-repos');
+exports.storeGithubRepos    =  require('./lib/store-github-repos');
+exports.updateGithubRepos   =  require('./lib/update-github-repos');
+exports.clientIdSecretQuery =  require('./lib/client-id-secret-query');
+exports.updateMultipleUsers =  require('./lib/update-multiple-github-users');
