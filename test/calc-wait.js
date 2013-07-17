7a91258a9c020fd4c2c4b25bc715c056546f73ee
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-16 21:25:08 -0400

    less specified calc-wait tests

--- test/calc-wait.js
+++ test/calc-wait.js
@@ -12,7 +12,9 @@ test('\nneed to wait for an hour and waited for 30 mins', function (t) {
   var d2 = Date.now();
   d2 -= min * 30;
 
-  t.equal(calc(d2, hour) / min, 30, 'need to wait for 30 mins more');
+  var wait = calc(d2, hour) / min;
+
+  t.ok(29 <= wait && wait <= 30, 'need to wait for 30 mins more');
   t.end()
 })
 
@@ -21,7 +23,9 @@ test('\nneed to wait for an hour and waited for 50 mins', function (t) {
   var d2 = Date.now();
   d2 -= min * 50;
 
-  t.equal(calc(d2, hour) / min, 10, 'need to wait for 10 mins more');
+  var wait = calc(d2, hour) / min;
+
+  t.ok(9 <= wait && wait <=  10, 'need to wait for 10 mins more');
   t.end()
 })
 
@@ -29,6 +33,8 @@ test('\nneed to wait for an hour and waited for 70 mins', function (t) {
   var d1 = Date.now();
   var d2 = Date.now();
   d2 -= min * 70;
+
+  var wait = calc(d2, hour) / min;
 
   t.equal(calc(d2, hour), 0, 'need to wait for 0 ms more');
   t.end()
